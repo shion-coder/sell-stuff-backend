cc7ad293e0a8a3a41f9b34eb313a23d18efdf5e6
Shion <shion.coder@gmail.com>
2020-07-19 16:44:53 +0700

    Init

--- routes/listings.js
+++ routes/listings.js
@@ -1,25 +1,25 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Joi = require("joi");
-const multer = require("multer");
+const Joi = require('joi');
+const multer = require('multer');
 
-const store = require("../store/listings");
-const categoriesStore = require("../store/categories");
-const validateWith = require("../middleware/validation");
-const auth = require("../middleware/auth");
-const imageResize = require("../middleware/imageResize");
-const delay = require("../middleware/delay");
-const listingMapper = require("../mappers/listings");
-const config = require("config");
+const store = require('../store/listings');
+const categoriesStore = require('../store/categories');
+const validateWith = require('../middleware/validation');
+const auth = require('../middleware/auth');
+const imageResize = require('../middleware/imageResize');
+const delay = require('../middleware/delay');
+const listingMapper = require('../mappers/listings');
+const config = require('config');
 
 const upload = multer({
-  dest: "uploads/",
+  dest: 'uploads/',
   limits: { fieldSize: 25 * 1024 * 1024 },
 });
 
 const schema = {
   title: Joi.string().required(),
-  description: Joi.string().allow(""),
+  description: Joi.string().allow(''),
   price: Joi.number().required().min(1),
   categoryId: Joi.number().required().min(1),
   location: Joi.object({
@@ -30,19 +30,19 @@ const schema = {
 
 const validateCategoryId = (req, res, next) => {
   if (!categoriesStore.getCategory(parseInt(req.body.categoryId)))
-    return res.status(400).send({ error: "Invalid categoryId." });
+    return res.status(400).send({ error: 'Invalid categoryId.' });
 
   next();
 };
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   const listings = store.getListings();
   const resources = listings.map(listingMapper);
   res.send(resources);
 });
 
 router.post(
-  "/",
+  '/',
   [
     // Order of these middleware matters.
     // "upload" should come before other "validate" because we have to handle
@@ -52,7 +52,7 @@ router.post(
     // stored in the uploads folder. We'll need to clean up this folder
     // using a separate process.
     // auth,
-    upload.array("images", config.get("maxImageCount")),
+    upload.array('images', config.get('maxImageCount')),
     validateWith(schema),
     validateCategoryId,
     imageResize,
@@ -65,6 +65,7 @@ router.post(
       categoryId: parseInt(req.body.categoryId),
       description: req.body.description,
     };
+
     listing.images = req.images.map((fileName) => ({ fileName: fileName }));
     if (req.body.location) listing.location = JSON.parse(req.body.location);
     if (req.user) listing.userId = req.user.userId;
